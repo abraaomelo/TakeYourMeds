3c4cfbc55906cca999d5a1978bb503fedb77bbe5
Melo Abraao <abraao.st.melo@gmail.com>
2022-07-07 22:06:48 -0300

    WIP: Database add medicines

---
+++ app/src/main/java/com/example/takeyourmeds/MedicineDAO.java
@@ -0,0 +1,23 @@
+package com.example.takeyourmeds;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+public class MedicineDAO {
+
+    private ConnectionDB connection;
+    private SQLiteDatabase db;
+
+    public MedicineDAO(Context context){
+        connection = new ConnectionDB(context);
+        db = connection.getWritableDatabase();
+    }
+
+    public long med_Create(Medicine medicine){
+        ContentValues values = new ContentValues();
+        values.put("med_name", medicine.getMedName());
+        return db.insert("medicine", null, values);
+    }
+
+}
